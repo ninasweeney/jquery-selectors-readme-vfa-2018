cd4707ac0f1eaa2e1d480e546795decc02c8caa7
Nina Sweeney <nsweeney@middlebury.edu>
2018-06-27 20:19:54 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,26 @@
 // declare your functions here...
+
+function paragraphSelector() {
+  answer= $('p')
+  return answer 
+}
+
+function lastImageSelector() {
+  answer= $('img:last')
+  return answer;
+}
+
+function ninjaBabySelector() {
+  answer= $('#baby-ninja')
+  return answer;
+}
+
+function divSelector() {
+  answer= $('.pics')
+  return answer;
+}
+
+function firstListItem() {
+  answer= $('#pic-list li:first-child')
+  return answer
+}
