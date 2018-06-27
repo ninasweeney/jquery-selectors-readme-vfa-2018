e808cda16eb9fe2ba5987bf70e70df1952e4a49b
Nina Sweeney <nsweeney@middlebury.edu>
2018-06-27 20:06:13 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -4,22 +4,3 @@ function paragraphSelector() {
   return $('p')
 }
 
-function lastImageSelector() {
-  answer= $('img:last')
-  return answer;
-}
-
-function ninjaBabySelector() {
-  answer= $('#baby-ninja')
-  return answer;
-}
-
-function divSelector() {
-  answer= $('.pics')
-  return answer;
-}
-
-function firstListItem() {
-  $('parent-tag child-tag:first-child')
-  answer= $('ul li:first-child')
-}
